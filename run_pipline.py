5c7213740972149267fe6f22961b80ba231d0938
yinonbaron <yinon.baron@gmail.com>
2018-04-04 15:30:20 +0300

    update

--- run_pipline.py
+++ run_pipline.py
@@ -9,6 +9,8 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 # Define function that runs a jypyter notebook and saves the results to the same file
 def run_nb(path):
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor
     with open(path) as f:
         nb = nbformat.read(f, as_version=4)
         ep = ExecutePreprocessor(timeout=6000, kernel_name='python3')
